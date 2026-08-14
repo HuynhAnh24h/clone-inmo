0269a7ab21709e33b8dd5bf037611fff0c46bef4
HuynhAnh24h <anhdev24h@gmail.com>
2026-08-14 10:43:31 +0700

    Huỳnh Anh Commit

--- template-parts/product/section-ready-cta.php
+++ template-parts/product/section-ready-cta.php
@@ -6,7 +6,7 @@ global $product;
 ?>
 <div class="pg-dark-section pg-ready" style="padding-top: 0">
 	<h2>Bạn Đã Sẵn Sàng Trải Nghiệm <?php echo esc_html($product->get_name()); ?>?</h2>
-	<button type="button" class="btn-pg-cta">
+	<a href="<?php echo esc_url( wc_get_checkout_url() ); ?>?add-to-cart=<?php echo esc_attr( $product->get_id() ); ?>" class="btn-pg-cta">
 		Mua Ngay <i class="bi bi-arrow-right ms-1"></i>
-	</button>
+	</a>
 </div>
